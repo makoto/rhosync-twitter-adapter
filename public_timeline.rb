485044f3f56ed3db5009645d97be4e1aa8617e69
Makoto Inoue <makoto@new-bamboo.co.uk>
2009-02-01 10:12:40 +0000

    Small refactoring

--- public_timeline.rb
+++ public_timeline.rb
@@ -470,6 +470,7 @@
 #     </user>
 #     </status>
 #     </statuses>
+
 module Twitter
   class PublicTimeline < SourceAdapter
   
@@ -480,48 +481,46 @@ module Twitter
     end
 
     def query
-      log "Twitter query"
+      log "#{self.class} query"
       log @source.url.inspect
 
       uri = URI.parse(@source.url)
       res = Net::HTTP.start(uri.host, uri.port) {|http|
         http.get("/statuses/public_timeline.xml")
       }
-      # @result = res.body    
       xml_data = XmlSimple.xml_in(res.body); 
       @result = xml_data["status"]
     end
 
     def sync
-      log "PublicTimeline sync, with #{@result.length} results"
+      log "#{self.class} sync, with #{@result.length} results"
       @result.each do |item|
         item_id = item["id"].first.to_i
-        iterate(:item => item, :item_id => item_id)
+        iterate_keys(:item => item, :item_id => item_id)
       end
     end
-  
+      
   private
-  def iterate(option)
-    # iterate over all possible values, if the value is not found we just pass "" in to rhosync
-    item = option[:item]
-    item_id = option[:item_id]
-    # for nested keys. Will be seperated by "_" : eg. user_name
-    prefix = option[:prefix] || ""
-    
-    item.keys.each do |key|
-      value = item[key] ? item[key][0] : ""
-      if value.kind_of?(Hash) && value != {}
-        iterate(:prefix => (underline(key) + "_"), :item => value, :item_id => item_id)
-      else
-        add_triple(@source.id, item_id, prefix + underline(key), value)
+    def iterate_keys(option)
+      item = option[:item]
+      item_id = option[:item_id]
+      prefix = option[:prefix] || ""
+
+      # item.keys => [\"user\", \"favorited\", \"truncated\"...]
+      item.keys.each do |key|
+        value = item[key] ? item[key][0] : ""
+        if value.kind_of?(Hash) && value != {}
+          # eg. :user => {:url => 'foo} becomes user_url
+          iterate_keys(:prefix => (prefix + underline(key) + "_"), :item => value, :item_id => item_id)
+        else
+          # This method is from rest_api_helper
+          add_triple(@source.id, item_id, prefix + underline(key), value)
+        end
       end
     end
-  end
 
-  def underline(key)
-    key.gsub('-','_')
-  end
-  
-    # not planning to create, update or delete projects on device
+    def underline(key)
+      key.gsub('-','_')
+    end
   end
 end
