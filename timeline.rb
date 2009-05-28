4aa683f0ae64935a33151557ba5c292ec98bd5c1
Makoto Inoue <makoto@new-bamboo.co.uk>
2009-05-28 22:42:30 +0100

    Changed timeline to use BaseTimeline

--- timeline.rb
+++ timeline.rb
@@ -4,84 +4,51 @@
 # Example Twitter REST source adapter for Friend TimeLine
 # Format is same as public timeline
 # See more detail at http://apiwiki.twitter.com/REST+API+Documentation
+require 'uri'
+require 'xmlsimple'
+require 'net/http'
 
-module Twitter
-  class Timeline < SourceAdapter
+  class Timeline < BaseTimeline
   
-    include RestAPIHelpers
-  
-    def initialize(source)
+    def initialize(source, credential = nil)
       super
     end
 
-    # Looks not in use
-    # def login
-    #   
-    # end
-
     def query
       url = "/statuses/friends_timeline.xml"
-      uri = URI.parse(@source.url+url)
+      uri = URI.parse(@source.credential.url+url)
 
       # Login authentication. The logic is taken from basecamp_projects.rb
       req = Net::HTTP::Get.new(uri.path, 'Accept' => 'application/xml')
-      req.basic_auth @source.login, @source.password
+      req.basic_auth @source.credential.login, @source.credential.password
 
       res = Net::HTTP.start(uri.host,uri.port) do |http|
         http.request(req)
       end
-      
-      xml_data = XmlSimple.xml_in(res.body); 
-      @result = xml_data["status"]
-    end
-
-    def sync
-      log "#{self.class} sync, with #{@result.length} results"
-      @result.each do |item|
-        item_id = item["id"].first.to_i
-        iterate_keys(:item => item, :item_id => item_id)
-      end
+      xml_data = XmlSimple.xml_in(res.body);
+      @nested_result = xml_data["status"]
     end
     
     def create(name_value_list)
-      url = "/statuses/update.json"
-      uri = URI.parse(@source.url+url)
-
-      name_value_list.each do |name_value|
-        value = name_value["value"] if name_value["name"]
-        
-        req = Net::HTTP::Post.new(uri.path, 'Accept' => 'application/xml')
-        req.basic_auth @source.login, @source.password
-
-        req.set_form_data('status'=> value)
-
-        res = Net::HTTP.start(uri.host,uri.port) do |http|
-          http.request(req)
-        end
-      end
-    end
+      url = "/statuses/update.xml"
+      uri = URI.parse(@source.credential.url+url)
       
-  private
-    def iterate_keys(option)
-      item = option[:item]
-      item_id = option[:item_id]
-      prefix = option[:prefix] || ""
-
-      # item.keys => [\"user\", \"favorited\", \"truncated\"...]
-      item.keys.each do |key|
-        value = item[key] ? item[key][0] : ""
-        if value.kind_of?(Hash) && value != {}
-          # eg. :user => {:url => 'foo} becomes user_url
-          iterate_keys(:prefix => (prefix + underline(key) + "_"), :item => value, :item_id => item_id)
-        else
-          # This method is from rest_api_helper
-          add_triple(@source.id, item_id, prefix + underline(key), value)
-        end
+      get_params(name_value_list)
+      
+      req = Net::HTTP::Post.new(uri.path, 'Accept' => 'application/xml')
+      req.basic_auth @source.credential.login, @source.credential.password
+    
+      req.set_form_data('status'=> params["text"])
+    
+      res = Net::HTTP.start(uri.host,uri.port) do |http|
+        http.request(req)
       end
     end
 
-    def underline(key)
-      key.gsub('-','_')
-    end
   end
-end
+ 
+ 
+ 
+ 
+ 
+ 
