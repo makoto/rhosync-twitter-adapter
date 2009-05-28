fe34f8f72dc061106cb7ed0e6ce9543f3c66042d
Makoto Inoue <makoto@new-bamboo.co.uk>
2009-05-28 21:18:06 +0100

    Changed to use base_timeline

---
+++ base_timeline.rb
@@ -0,0 +1,33 @@
+  class BaseTimeline < SourceAdapter
+  
+      
+  private
+    def iterate_keys(option)
+      item = option[:item]
+      item_id = option[:item_id]
+      prefix = option[:prefix] || ""
+      row_num = option[:row_num].to_i
+      # raise row_num.class.inspect.
+      
+      @result[row_num] ||= {}
+      # item.keys => ["user", "favorited", "truncated"...]
+      item.keys.each do |key|
+        # value = item[key] ? item[key] : ""
+        value = item[key] ? item[key][0] : ""
+        if value.kind_of?(Hash) && value != {}
+          # eg. :user => {:url => 'foo} becomes user_url
+          iterate_keys(:prefix => (prefix + underline(key) + "_"), :item => value, :item_id => item_id, :key => key)
+        else
+          # raise item[key][0].inspect
+          # This method is from rest_api_helper
+          @result[row_num][prefix + underline(key)] = value
+          # add_triple(@source.id, item_id, prefix + underline(key), value,  @source.current_user.id)
+        end
+      end
+    end
+
+    def underline(key)
+      key.gsub('-','_')
+    end
+  end
+# end
--- public_timeline.rb
+++ public_timeline.rb
@@ -477,7 +477,7 @@ require 'uri'
 require 'xmlsimple'
 require 'net/http'
 
-  class PublicTimeline < SourceAdapter
+  class PublicTimeline < BaseTimeline
   
     # include RestAPIHelpers
   
@@ -521,33 +521,33 @@ require 'net/http'
     #   end
     # end
       
-  private
-    def iterate_keys(option)
-      item = option[:item]
-      item_id = option[:item_id]
-      prefix = option[:prefix] || ""
-      row_num = option[:row_num].to_i
-      # raise row_num.class.inspect.
-      
-      @result[row_num] ||= {}
-      # item.keys => ["user", "favorited", "truncated"...]
-      item.keys.each do |key|
-        # value = item[key] ? item[key] : ""
-        value = item[key] ? item[key][0] : ""
-        if value.kind_of?(Hash) && value != {}
-          # eg. :user => {:url => 'foo} becomes user_url
-          iterate_keys(:prefix => (prefix + underline(key) + "_"), :item => value, :item_id => item_id, :key => key)
-        else
-          # raise item[key][0].inspect
-          # This method is from rest_api_helper
-          @result[row_num][prefix + underline(key)] = value
-          # add_triple(@source.id, item_id, prefix + underline(key), value,  @source.current_user.id)
-        end
-      end
-    end
-
-    def underline(key)
-      key.gsub('-','_')
-    end
+  # private
+  #   def iterate_keys(option)
+  #     item = option[:item]
+  #     item_id = option[:item_id]
+  #     prefix = option[:prefix] || ""
+  #     row_num = option[:row_num].to_i
+  #     # raise row_num.class.inspect.
+  #     
+  #     @result[row_num] ||= {}
+  #     # item.keys => ["user", "favorited", "truncated"...]
+  #     item.keys.each do |key|
+  #       # value = item[key] ? item[key] : ""
+  #       value = item[key] ? item[key][0] : ""
+  #       if value.kind_of?(Hash) && value != {}
+  #         # eg. :user => {:url => 'foo} becomes user_url
+  #         iterate_keys(:prefix => (prefix + underline(key) + "_"), :item => value, :item_id => item_id, :key => key)
+  #       else
+  #         # raise item[key][0].inspect
+  #         # This method is from rest_api_helper
+  #         @result[row_num][prefix + underline(key)] = value
+  #         # add_triple(@source.id, item_id, prefix + underline(key), value,  @source.current_user.id)
+  #       end
+  #     end
+  #   end
+  # 
+  #   def underline(key)
+  #     key.gsub('-','_')
+  #   end
   end
 # end
